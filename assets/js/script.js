4d3d04cc0b3786c7a3b82256e53783c5fd53d422
Dylan Thomson <djthomson88@gmail.com>
2017-07-07 13:49:31 -0400

    Get stream data
    -After getting channel data, make request for stream data using another
    API call
    -If there is stream data, display the stream's status
    -Otherwise, display "offline"
    -Move DOM manipulation logic outside of getChannel using callbacks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,47 @@
-// https://wind-bow.gomix.me/twitch-api
-
+// Setup popular channels array
 var popularChannels = ["dansgaming", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 // Document Ready
 $(function() {
-	// getPopularStream("dansgaming");
 	popularChannels.forEach(function(channel) {
-		getPopularChannel(channel);
+		// Get channel data for current channel
+		getChannel(channel, function(data) {
+			var currentChannel = data;
+
+			// Get Stream data (if any) for current channel
+			getStream(channel, function(data) {
+				var currentStream = data;
+				// Display online/offline status - if there is no stream data, channel is offline
+				if(currentStream.stream) {
+					$("#popular").append("<div>" + currentChannel.display_name + " - " + currentStream.stream.stream_type + "</div>");				
+				}
+				else {
+					$("#popular").append("<div>" + currentChannel.display_name + " - " + "offline" + "</div>");				
+				}
+			});
+		});
 	});
 });
 
-function getPopularChannel(channel) {
+// Request Channel Data
+function getChannel(channel, callback) {
+	// Using wind-bow because Twitch.tv API now requires a key and this is a public repo
 	var url = "https://wind-bow.gomix.me/twitch-api/channels/" + channel + "?callback=?";
 	$.getJSON(url, function(data) {
-		$("#popular").append("<div>" + data.display_name + " - " + data.status + "</div>");
+		if(callback) {
+			callback(data);
+		}
 	});
+}
+
+// Request Stream data
+function getStream(channel, callback) {
+	// Using wind-bow because Twitch.tv API now requires a key and this is a public repo
+	var url = "https://wind-bow.gomix.me/twitch-api/streams/" + channel + "?callback=?";
+	$.getJSON(url, function(data) {
+		if(callback) {
+			callback(data);
+		}
+	});
+
 }
