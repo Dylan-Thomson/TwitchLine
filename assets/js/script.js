bdf19d3ca7fe2470f6e3c640ea8f367d3f8e19d1
Dylan Thomson <djthomson88@gmail.com>
2017-07-08 12:53:40 -0400

    Name Changes
    -Change names to initPopular and initFeatured
    -Add lirik as a popular streamer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
 // Document Ready
 $(function() {
-	displayPopularChannels();
-	getFeaturedStreams();
+	initPopular();
+	initFeatured();
 });
 
 /*******************************************************************************************************************************
 	Logic for popular channels
 *******************************************************************************************************************************/
 // Setup popular channels array - comster404 and brunofin are nonexistent accounts for testing
-var popularChannels = ["dansgaming", "brunofin", "comster404", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var popularChannels = ["dansgaming", "lirik", "brunofin", "comster404", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 // Get channel and stream data for popular channels and update div with id #popular
-function displayPopularChannels() {	
+function initPopular() {	
 	popularChannels.forEach(function(channel) {
 		// Get channel data for current channel
 		getPopularChannel(channel, function(data) {
@@ -61,15 +61,11 @@ function getPopularStream(channel, callback) {
 /*******************************************************************************************************************************
 	Logic for featured streams
 *******************************************************************************************************************************/
-function getFeaturedStreams() {
+function initFeatured() {
 	var url = "https://wind-bow.glitch.me/twitch-api/streams/featured?callback=?";
 	$.getJSON(url, function(data) {
-		displayFeaturedStreams(data);
+		for(var i=0; i<data.featured.length; i++) {
+			$("#featured").append("<div>" + data.featured[i].stream.channel.display_name + " is playing " + data.featured[i].stream.game + "</div>");	
+		}
 	});
 }
-
-function displayFeaturedStreams(data) {
-	for(var i=0; i<data.featured.length; i++) {
-		$("#featured").append("<div>" + data.featured[i].stream.channel.display_name + " is playing " + data.featured[i].stream.game + "</div>");	
-	}
-} 
