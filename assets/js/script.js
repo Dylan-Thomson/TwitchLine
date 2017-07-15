329d1adf1468d9c1a2fa8f45c34b437229502644
Dylan Thomson <djthomson88@gmail.com>
2017-07-14 22:09:25 -0400

    Remove comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,6 @@ $(function() {
 *******************************************************************************************************************************/
 // Setup popular channels array - comster404 and brunofin are nonexistent accounts for testing
 var popularChannels = ["dansgaming", "strippin", "sheriffeli", "dexbonus", "lirik", "moonmoon_ow", "comster404", "bobross", "timthetatman", "riotgames", "crendor", "shaboozey", "freecodecamp", "esl_csgo", "nl_kripp", "trump", "totalbiscuit"];
-// var popularChannels = ["dansgaming", "strippin", "sheriffeli", "comster404", "freecodecamp"];
 // Get channel and stream data for popular channels and update div with id #popular
 function initPopular() {	
 	popularChannels.forEach(function(channel) {
